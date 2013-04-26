07867aec62053b25e5da9338266cc2006c2697a9
xtechgeek <xclusive585@gmail.com>
2013-04-26 03:56:53 -0300

    Create sscom.ino

---
+++ sscom.ino
@@ -0,0 +1,30 @@
+String cwrd = "";
+char cchar;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Ready: Try \"test\" command.");
+}
+
+void loop() {
+  while (Serial.available()) {
+    cchar = Serial.read();
+    cwrd.concat(cchar); delay (10);
+  }
+  if (cwrd=="test") {
+    Serial.println("\"test\" successul, try \"another command\".");
+    cwrd = "";
+  }
+  if (cwrd=="another command") {
+    Serial.println("it works!");
+    cwrd = "";
+  }
+  if (cwrd=="help") {
+    Serial.println("available commands are: \"help\", \"test\", and \"another command\".");
+    cwrd = "";
+  }
+  else if (cwrd != "") {
+    Serial.print("unknown command: \"");Serial.print(cwrd);Serial.println("\"");
+    cwrd = "";
+  }
+}
