0d6cec499775f8c8883d8f3d631e922ed656820c
xtechgeek <xclusive585@gmail.com>
2013-04-27 22:51:18 -0300

    Update sscom.ino

--- sscom.ino
+++ sscom.ino
@@ -1,6 +1,7 @@
 /* Released under GPL. As is without warranty. Work in progress with much left to do.
-/* Currently only tested for mega2560. Also only supports Arduino IDE's Serial Monitor, or other monitors that allow
-/* sending of more than one byte at a time.
+/* Currently only tested for mega2560. Also only supports Arduino IDE's Serial Monitor, 
+/* or other monitors that allow sending of more than one byte at a time.
+/* 
 /* Original sketch by dave@xtechgeek.com 4/2013
 /*
 /* version 0.03 */
